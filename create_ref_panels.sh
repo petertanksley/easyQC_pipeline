fc29685c0c7b929c1f287eeef45323784cd3f000
Peter Tanksley <tankslpr@login2.ls6.tacc.utexas.edu>
2023-12-15 13:47:03 -0600

    made ref panel script more flexible; changed MAF threshold to .005

--- create_ref_panels.sh
+++ create_ref_panels.sh
@@ -28,25 +28,39 @@ TEMP_REF="../temp/ref"
 
 mkdir -p $TEMP_REF
 
+#set MAF thesholds
+AFR_MAF="0.005"
+AFR_MAF_NAME="maf005"
+
+EUR_MAF="$AFR_MAF"
+EUR_MAF_NAME="$AFR_MAF_NAME"
+
 #=AFR REF PANEL==========================================================================#
 
-if [ ! -f $TEMP_REF/1KG_AFR_AF_MAF01_rsid.txt ]; then
+if [ ! -f "$TEMP_REF/1KG_AFR_AF_${AFR_MAF_NAME}_rsid.txt" ]; then
 
-grep -vFf $REL_IDS $PSAM_RAW | awk '$5 == "AFR" {print $1}' > $TEMP_REF/all_phase3_king2d_afr.txt
+#find all subject IDs that were not reported as 2nd degree relatives
+#select only individuals belonging to the AFR superpopulation
+grep -vFf "$REL_IDS" "$PSAM_RAW" | awk '$5 == "AFR" {print $1}' > "$TEMP_REF/all_phase3_king2d_afr.txt"
 
-
-plink2 	--pfile $PGEN_RAW \
-	--keep $TEMP_REF/all_phase3_king2d_afr.txt \
+#keep individuals identified above
+#keep only biallelic SNPs (no indels)
+#restrict to SNPs with greater than 0.005 MAF
+plink2 	--pfile "$PGEN_RAW" \
+	--keep "$TEMP_REF/all_phase3_king2d_afr.txt" \
 	--snps-only just-acgt \
-	--maf 0.01 \
+	--maf "$AFR_MAF" \
 	--make-pgen \
-	--out $TEMP_REF/all_phase3_king2d_afr_snps_maf01
+	--out "$TEMP_REF/all_phase3_king2d_afr_snps_${AFR_MAF_NAME}"
 
-plink2	--pfile $TEMP_REF/all_phase3_king2d_afr_snps_maf01 \
+#get frequency file
+plink2	--pfile "$TEMP_REF/all_phase3_king2d_afr_snps_${AFR_MAF_NAME}" \
 	--freq \
-	--out $TEMP_REF/all_phase3_king2d_afr_snps_maf01
+	--out "$TEMP_REF/all_phase3_king2d_afr_snps_${AFR_MAF_NAME}"
 
-awk 'BEGIN {OFS="\t"} !/^##/{print $2, $3}' $TEMP_REF/all_phase3_king2d_afr_snps_maf01.pvar > $TEMP_REF/positions_afr.txt
+
+#extract SNP info
+awk 'BEGIN {OFS="\t"} !/^##/{print $2, $3}' "$TEMP_REF/all_phase3_king2d_afr_snps_${AFR_MAF_NAME}.pvar" > "$TEMP_REF/positions_afr.txt"
 
 #check that rows were kept consistent
 #cut -f2 ../temp/ref/all_phase3_king2d_afr_snps_maf01.afreq | head
@@ -74,7 +88,7 @@ awk 'BEGIN {OFS="\t"} !/^##/{print $2, $3}' $TEMP_REF/all_phase3_king2d_afr_snps
 #rs546169444
 
 #Paste files together to add position data to freq file
-paste $TEMP_REF/all_phase3_king2d_afr_snps_maf01.afreq $TEMP_REF/positions_afr.txt > $TEMP_REF/all_phase3_king2d_afr_snps_maf01_freq.txt
+paste "$TEMP_REF/all_phase3_king2d_afr_snps_${AFR_MAF_NAME}.afreq" "$TEMP_REF/positions_afr.txt" > "$TEMP_REF/all_phase3_king2d_afr_snps_${AFR_MAF_NAME}_freq.txt"
 
 #double check that rows were correctly aligned
 
@@ -85,100 +99,83 @@ paste $TEMP_REF/all_phase3_king2d_afr_snps_maf01.afreq $TEMP_REF/positions_afr.t
 awk 'BEGIN {OFS="\t"} \
 	NR==1 {print "ChrPosID", "Chr", "RSID", "Pos", "REF", "ALT", "AF_ALT_AFR_1000G"} \
 	NR>1 {print $1":"$7, $1, $2, $7, $3, $4, $5}' \
-	$TEMP_REF/all_phase3_king2d_afr_snps_maf01_freq.txt > $TEMP_REF/1KG_AFR_AF_MAF01.txt
+	"$TEMP_REF/all_phase3_king2d_afr_snps_${AFR_MAF_NAME}_freq.txt" > "$TEMP_REF/1KG_AFR_AF_${AFR_MAF_NAME}.txt"
 
 
 #wc -l ../temp/ref/1KG_AFR_AF_MAF01.txt
 #15482394 ../temp/ref/1KG_AFR_AF_MAF01.txt
 
-#head -n2 ../temp/ref/1KG_AFR_AF_MAF01.txt "ChrPosID", "Chr", "RSID", "Pos", "REF", "ALT", "AF_ALT_AFR_1000G"
+#head -n2 ../temp/ref/1KG_AFR_AF_MAF01.txt
 #ChrPosID       Chr     RSID    Pos     REF     ALT     AF_ALT_AFR_1000G
 #1:10642        1       rs558604819     10642   G       A       0.0130568
 
 #split reference file into RSID and ALLELE files
   # RSID file (RSID, CHROM, POS)
-  awk -F'\t' -v OFS='\t' '{print $3, $2, $4}' $TEMP_REF/1KG_AFR_AF_MAF01.txt > $TEMP_REF/1KG_AFR_AF_MAF01_rsid.txt
+  awk -F'\t' -v OFS='\t' '{print $3, $2, $4}' "$TEMP_REF/1KG_AFR_AF_${AFR_MAF_NAME}.txt" > "$TEMP_REF/1KG_AFR_AF_${AFR_MAF_NAME}_rsid.txt"
 
   # ALLELE file (ChrPosID, REF, ALT, AF_ALT_AFR_1000G)
-  awk -F'\t' -v OFS='\t' '{print $1, $5, $6, $7}' $TEMP_REF/1KG_AFR_AF_MAF01.txt > $TEMP_REF/1KG_AFR_AF_MAF01_allele.txt
+  awk -F'\t' -v OFS='\t' '{print $1, $5, $6, $7}' "$TEMP_REF/1KG_AFR_AF_${AFR_MAF_NAME}.txt" > "$TEMP_REF/1KG_AFR_AF_${AFR_MAF_NAME}_allele.txt"
 
 fi
 
 
+
+
+
+
 #=EUR REF PANEL==========================================================================#
 
-if [ ! -f $TEMP_REF/1KG_EUR_AF_MAF001_rsid.txt ]; then
+if [ ! -f "$TEMP_REF/1KG_EUR_AF_${EUR_MAF_NAME}_rsid.txt" ]; then
 
-grep -vFf $REL_IDS $PSAM_RAW | awk '$5 == "EUR" {print $1}' > $TEMP_REF/all_phase3_king2d_eur.txt
+#find all subject IDs that were not reported as 2nd degree relatives
+#select only individuals belonging to the EUR superpopulation
+grep -vFf "$REL_IDS" "$PSAM_RAW" | awk '$5 == "EUR" {print $1}' > "$TEMP_REF/all_phase3_king2d_eur.txt"
 
-
-plink2  --pfile $PGEN_RAW \
-        --keep $TEMP_REF/all_phase3_king2d_eur.txt \
+#keep individuals identified above
+#keep only biallelic SNPs (no indels)
+#restrict to SNPs with greater than 0.005 MAF
+plink2  --pfile "$PGEN_RAW" \
+        --keep "$TEMP_REF/all_phase3_king2d_eur.txt" \
         --snps-only just-acgt \
-        --maf 0.001 \
+        --maf "$EUR_MAF" \
         --make-pgen \
-        --out $TEMP_REF/all_phase3_king2d_eur_snps_maf001
+        --out "$TEMP_REF/all_phase3_king2d_eur_snps_${EUR_MAF_NAME}"
 
-plink2  --pfile $TEMP_REF/all_phase3_king2d_eur_snps_maf001 \
+#get frequency file
+plink2  --pfile "$TEMP_REF/all_phase3_king2d_eur_snps_${EUR_MAF_NAME}" \
         --freq \
-        --out $TEMP_REF/all_phase3_king2d_eur_snps_maf001
+        --out "$TEMP_REF/all_phase3_king2d_eur_snps_${EUR_MAF_NAME}"
 
-awk 'BEGIN {OFS="\t"} !/^##/{print $2, $3}' $TEMP_REF/all_phase3_king2d_eur_snps_maf001.pvar > $TEMP_REF/positions_eur.txt
 
-#check that rows were kept consistent
-#cut -f2 ../temp/ref/all_phase3_king2d_eur_snps_maf001.afreq | head
-#ID
-#rs575272151
-#rs544419019
-#rs540538026
-#rs62635286
-#rs200579949
-#rs531730856
-#rs574697788
-#rs554008981
-#rs546169444
+#extract SNP info
+awk 'BEGIN {OFS="\t"} !/^##/{print $2, $3}' "$TEMP_REF/all_phase3_king2d_eur_snps_${EUR_MAF_NAME}.pvar" > "$TEMP_REF/positions_eur.txt"
 
-#cut -f2 ../temp/ref/positions_eur.txt | head
-#ID
-#rs575272151
-#rs544419019
-#rs540538026
-#rs62635286
-#rs200579949
-#rs531730856
-#rs574697788
-#rs554008981
-#rs546169444
 
 #Paste files together to add position data to freq file
-paste $TEMP_REF/all_phase3_king2d_eur_snps_maf001.afreq $TEMP_REF/positions_eur.txt > $TEMP_REF/all_phase3_king2d_eur_snps_maf001_freq.txt
+paste "$TEMP_REF/all_phase3_king2d_eur_snps_${EUR_MAF_NAME}.afreq" "$TEMP_REF/positions_eur.txt" > "$TEMP_REF/all_phase3_king2d_eur_snps_${EUR_MAF_NAME}_freq.txt"
 
 #double check that rows were correctly aligned
 
-#awk '{ if ($2 != $8) { print } }' ../temp/ref/all_phase3_king2d_eur_snps_maf001_freq.txt
+#awk '{ if ($2 != $8) { print } }' "$TEMP_REF/all_phase3_king2d_eur_snps_${EUR_MAF_NAME}_freq.txt"
 #[no rows returned]
 
 #final formatting
 awk 'BEGIN {OFS="\t"} \
         NR==1 {print "ChrPosID", "Chr", "RSID", "Pos", "REF", "ALT", "AF_ALT_EUR_1000G"} \
         NR>1 {print $1":"$7, $1, $2, $7, $3, $4, $5}' \
-        $TEMP_REF/all_phase3_king2d_eur_snps_maf001_freq.txt > $TEMP_REF/1KG_EUR_AF_MAF001.txt
+        "$TEMP_REF/all_phase3_king2d_eur_snps_${EUR_MAF_NAME}_freq.txt" > "$TEMP_REF/1KG_EUR_AF_${EUR_MAF_NAME}.txt"
 
 
-#wc -l ../temp/ref/1KG_EUR_AF_MAF001.txt
-#15482394 ../temp/ref/1KG_EUR_AF_MAF001.txt
+#wc -l
 
-#head -n2 ../temp/ref/1KG_EUR_AF_MAF001.txt "ChrPosID", "Chr", "RSID", "Pos", "REF", "ALT", "AF_ALT_EUR_1000G"
-#ChrPosID       Chr     RSID    Pos     REF     ALT     AF_ALT_EUR_1000G
-#1:11008	1	rs575272151	11008	C	G	0.0884692
+#head -n2
 
 #split reference file into RSID and ALLELE files
   # RSID file (RSID, CHROM, POS)
-  awk -F'\t' -v OFS='\t' '{print $3, $2, $4}' $TEMP_REF/1KG_EUR_AF_MAF001.txt > $TEMP_REF/1KG_EUR_AF_MAF001_rsid.txt
+  awk -F'\t' -v OFS='\t' '{print $3, $2, $4}' "$TEMP_REF/1KG_EUR_AF_${EUR_MAF_NAME}.txt" > "$TEMP_REF/1KG_EUR_AF_${EUR_MAF_NAME}_rsid.txt"
 
-  # ALLELE file (ChrPosID, REF, ALT, AF_ALT_EUR_1000G)
-  awk -F'\t' -v OFS='\t' '{print $1, $5, $6, $7}' $TEMP_REF/1KG_EUR_AF_MAF001.txt > $TEMP_REF/1KG_EUR_AF_MAF001_allele.txt
-
+  # ALLELE file (ChrPosID, REF, ALT, AF_ALT_AFR_1000G)
+  awk -F'\t' -v OFS='\t' '{print $1, $5, $6, $7}' "$TEMP_REF/1KG_EUR_AF_${EUR_MAF_NAME}.txt" > "$TEMP_REF/1KG_EUR_AF_${EUR_MAF_NAME}_allele.txt"
 
 fi
 
